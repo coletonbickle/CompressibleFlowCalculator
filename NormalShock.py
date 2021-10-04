8b1cc64a4b2acd0cba5b5923dffd4265885b2e4f
Coleton Bickle <cbickle@purdue.edu>
2021-10-04 14:04:57 -0400

    I further optimized backwards calculations. Now calculates all backward calculates to the correct values in a faster time interval as well as in less lines of code.

--- NormalShock.py
+++ NormalShock.py
@@ -20,31 +20,35 @@ def main():
     e = "p2/p1"
     f = "rho2/rho1"
     h = "t2/t1"
+    k = 1
 
-    z = input("Enter input: ").lower()
-    n = float(input("Enter Value: "))
-    g = float(input("Enter Gamma Value: "))
+    while k == 1:
+        print("Please input any of the following: M1, M2, p02/p01, p2/p1, rho2/rho1, t2/t1\n")
+        z = input("Enter input: ").lower()
+        n = float(input("Enter Value: "))
+        g = float(input("Enter Gamma Value: "))
 
-    i = z == a or z == b or z == c or z == e or z == f or z == h  # or z == d
-    j = g > 1
-    if i and j:
-        fcn(z, n, g)
-
-    if _Normal.m1 is not None:
-        print(f"\nM1: {_Normal.m1:8.8f} M2: {_Normal.m2:8.8f} p02/01: {_Normal.p02p01:8.8f}")
-        print(f"p2/p1: {_Normal.p2p1:8.8f} rho2/rho1: {_Normal.r2r1:8.8f} T2/T1: {_Normal.t2t1:8.8f}")
-    else:
-        print(f"\nM1: {_Normal.m1} M2: {_Normal.m2} p02/01: {_Normal.p02p01}")
-        print(f"p2/p1: {_Normal.p2p1} rho2/rho1: {_Normal.r2r1} T2/T1: {_Normal.t2t1}")
-
-    print("Hello World")
+        i = z == a or z == b or z == c or z == e or z == f or z == h  # or z == d
+        j = g > 1
+        if i and j:
+            fcn(z, n, g)
+            k = 0
+            if _Normal.m1 is not None:
+                print(f"\nM1: {_Normal.m1:8.8f} M2: {_Normal.m2:8.8f} p02/01: {_Normal.p02p01:8.8f}")
+                print(f"p2/p1: {_Normal.p2p1:8.8f} rho2/rho1: {_Normal.r2r1:8.8f} T2/T1: {_Normal.t2t1:8.8f}")
+            else:
+                print(f"\nM1: {_Normal.m1} M2: {_Normal.m2} p02/01: {_Normal.p02p01}")
+                print(f"p2/p1: {_Normal.p2p1} rho2/rho1: {_Normal.r2r1} T2/T1: {_Normal.t2t1}\n")
+                print(_Normal.msg)
+        else:
+            print("\nError: Please input a valid input\n")
 
 
 def fcn(z, n, g):
     # Find out how to Calculate p1/p02
 
     # This Function includes all possible outcomes given possible inputs.
-    # Multiple nested while-loops that optimize backward calculation time to calculate M1 for each input (excluding M1)
+    # Multiple while-loops that optimize backward calculation time to calculate M1 for each input (excluding M1)
     if z == 'm1':
         m = n
         _Normal.m1 = n
@@ -57,50 +61,40 @@ def fcn(z, n, g):
     elif z == 'm2':
         _Normal.m2 = n
         m = 1
-        while m > 0:  # Optimized nested while-loop to Calculate M1
-            temp = math.sqrt(((g - 1) * m ** 2 + 2) / ((2 * g * m ** 2) - (g - 1)))
-            if temp <= n:
-                m = m - 100
-                while m > 0:
-                    temp = math.sqrt(((g - 1) * m ** 2 + 2) / ((2 * g * m ** 2) - (g - 1)))
-                    if temp <= n:
-                        m = m - 1
-                        while m > 0:
-                            temp = math.sqrt(((g - 1) * m ** 2 + 2) / ((2 * g * m ** 2) - (g - 1)))
-                            if temp <= n:
-                                m = m - 0.00001
-                                break
-                            else:
-                                m = m + 0.00001
+        increment = 100
+        if 0.3779644739999999 < n < 1:
+            while m > 0:  # Optimized while-loop to Calculate M1
+                temp = math.sqrt(((g - 1) * m ** 2 + 2) / ((2 * g * m ** 2) - (g - 1)))
+                if temp <= n:
+                    m = m - increment
+                    increment = increment/10
+                    if increment < 0.000001:
                         break
-                    else:
-                        m = m + 1
-                break
-            else:
-                m = m + 100
-        _Normal.m1 = m
-        _Normal.p2p1 = (2*g*m**2-(g-1))/(g+1)
-        _Normal.t2t1 = (2*g*m**2-(g-1))*((g-1)*m**2+2)/((g+1)**2*m**2)
-        _Normal.p02p01 = ((((g+1)*m**2)/((g-1)*m**2+2))**(g/(g-1)))*(((g+1)/(2*g*m**2-(g-1)))**(1/(g-1)))
-        _Normal.r2r1 = ((g+1)*m**2)/((g-1)*m**2+2)
+                else:
+                    m = m + increment
+            _Normal.m1 = m
+            _Normal.p2p1 = (2*g*m**2-(g-1))/(g+1)
+            _Normal.t2t1 = (2*g*m**2-(g-1))*((g-1)*m**2+2)/((g+1)**2*m**2)
+            _Normal.p02p01 = ((((g+1)*m**2)/((g-1)*m**2+2))**(g/(g-1)))*(((g+1)/(2*g*m**2-(g-1)))**(1/(g-1)))
+            _Normal.r2r1 = ((g+1)*m**2)/((g-1)*m**2+2)
+        else:
+            _error()
+            _Normal.msg = 'M2 Must be Between 0.377964474 and 1'
 
     elif z == 'p2/p1':
         _Normal.p2p1 = n
         m = 1
-        if n <= 10000000000:  # Optimized nested while-loop to Calculate M1
-            while m > 0:
+        increment = 100
+        if n <= 10000000000:
+            while m > 0:  # Optimized while-loop to Calculate M1
                 temp = (2 * g * m ** 2 - (g - 1)) / (g + 1)
-                if temp >= _Normal.p2p1:
-                    m = m - 1
-                    while m > 0:
-                        temp = (2 * g * m ** 2 - (g - 1)) / (g + 1)
-                        if temp >= _Normal.p2p1:
-                            break
-                        else:
-                            m = m + 0.00001
-                    break
+                if temp >= n:
+                    m = m - increment
+                    increment = increment / 10
+                    if increment < 0.000001:
+                        break
                 else:
-                    m = m + 1
+                    m = m + increment
             _Normal.m1 = m
             _Normal.m2 = math.sqrt(((g-1)*m**2+2)/((2*g*m**2)-(g-1)))
             _Normal.t2t1 = (2*g*m**2-(g-1))*((g-1)*m**2+2)/((g+1)**2*m**2)
@@ -113,72 +107,40 @@ def fcn(z, n, g):
     elif z == 'p02/p01':
         _Normal.p02p01 = n
         m = 1
-        while m > 0:  # Optimized nested while-loop to Calculate M1
-            temp = ((((g+1)*m**2)/((g-1)*m**2+2))**(g/(g-1)))*(((g+1)/(2*g*m**2-(g-1)))**(1/(g-1)))
-            if temp <= _Normal.p02p01:
-                m = m - 1
-                while m > 0:
-                    temp = ((((g + 1) * m ** 2) / ((g - 1) * m ** 2 + 2)) ** (g / (g - 1))) * (
-                                ((g + 1) / (2 * g * m ** 2 - (g - 1))) ** (1 / (g - 1)))
-                    if temp <= _Normal.p02p01:
+        increment = 10
+        if 0 < n < 1:
+            while m > 0:  # Optimized while-loop to Calculate M1
+                temp = ((((g+1)*m**2)/((g-1)*m**2+2))**(g/(g-1)))*(((g+1)/(2*g*m**2-(g-1)))**(1/(g-1)))
+                if temp <= n:
+                    m = m - increment
+                    increment = increment / 10
+                    if increment < 0.000001:
                         break
-                    else:
-                        m = m + 0.00001
-                break
-            else:
-                m = m + 1
-        _Normal.m1 = m
-        _Normal.m2 = math.sqrt(((g - 1) * m ** 2 + 2) / ((2 * g * m ** 2) - (g - 1)))
-        _Normal.p2p1 = (2 * g * m ** 2 - (g - 1)) / (g + 1)
-        _Normal.t2t1 = (2 * g * m ** 2 - (g - 1)) * ((g - 1) * m ** 2 + 2) / ((g + 1) ** 2 * m ** 2)
-        _Normal.r2r1 = ((g + 1) * m ** 2) / ((g - 1) * m ** 2 + 2)
+                else:
+                    m = m + increment
+            _Normal.m1 = m
+            _Normal.m2 = math.sqrt(((g - 1) * m ** 2 + 2) / ((2 * g * m ** 2) - (g - 1)))
+            _Normal.p2p1 = (2 * g * m ** 2 - (g - 1)) / (g + 1)
+            _Normal.t2t1 = (2 * g * m ** 2 - (g - 1)) * ((g - 1) * m ** 2 + 2) / ((g + 1) ** 2 * m ** 2)
+            _Normal.r2r1 = ((g + 1) * m ** 2) / ((g - 1) * m ** 2 + 2)
+        else:
+            _error()
+            _Normal.msg = "p02/p01 Must be Between 0 and 1"
 
     elif z == 'rho2/rho1':
         _Normal.r2r1 = n
         m = 1
+        increment = 100000
         if 1 <= n <= 6:
-            if 1 <= n <= 5.9999999:  # Optimized nested while-loop to Calculate M1
-                while m > 0:
-                    temp = ((g + 1) * m ** 2) / ((g - 1) * m ** 2 + 2)
-                    if temp >= n:
-                        m = m - 100
-                        while m > 0:
-                            temp = ((g + 1) * m ** 2) / ((g - 1) * m ** 2 + 2)
-                            if temp >= n:
-                                m = m - 1
-                                while m > 0:
-                                    temp = ((g + 1) * m ** 2) / ((g - 1) * m ** 2 + 2)
-                                    if temp >= n:
-                                        break
-                                    else:
-                                        m = m + 0.00001
-                                break
-                            else:
-                                m = m + 1
+            while m > 0:  # Optimized while-loop to Calculate M1
+                temp = ((g + 1) * m ** 2) / ((g - 1) * m ** 2 + 2)
+                if temp >= n:
+                    m = m - increment
+                    increment = increment / 10
+                    if increment < 0.000001:
                         break
-                    else:
-                        m = m + 100
-            elif 5.9999999 < n <= 6:  # Optimized nested while-loop to Calculate M1
-                while m > 0:
-                    temp = ((g + 1) * m ** 2) / ((g - 1) * m ** 2 + 2)
-                    if temp >= n:
-                        m = m - 10000
-                        while m > 0:
-                            temp = ((g + 1) * m ** 2) / ((g - 1) * m ** 2 + 2)
-                            if temp >= n:
-                                m = m - 1
-                                while m > 0:
-                                    temp = ((g + 1) * m ** 2) / ((g - 1) * m ** 2 + 2)
-                                    if temp >= n:
-                                        break
-                                    else:
-                                        m = m + 0.00001
-                                break
-                            else:
-                                m = m + 1
-                        break
-                    else:
-                        m = m + 10000
+                else:
+                    m = m + increment
             _Normal.m1 = m
             _Normal.m2 = math.sqrt(((g-1)*m**2+2)/((2*g*m**2)-(g-1)))
             _Normal.p2p1 = (2 * g * m ** 2 - (g - 1)) / (g + 1)
@@ -191,20 +153,17 @@ def fcn(z, n, g):
     elif z == 't2/t1':
         _Normal.t2t1 = n
         m = 1
+        increment = 1000
         if 1 <= n <= 1000000000:
-            while m is True:  # Optimized nested while-loop to Calculate M1
+            while m > 0:  # Optimized while-loop to Calculate M1
                 temp = (2*g*m**2-(g-1))*((g-1)*m**2+2)/((g+1)**2*m**2)
                 if temp >= n:
-                    m = m - 1
-                    while m is True:
-                        temp = (2 * g * m ** 2 - (g - 1)) * ((g - 1) * m ** 2 + 2) / ((g + 1) ** 2 * m ** 2)
-                        if temp >= n:
-                            break
-                        else:
-                            m = m + 0.00001
-                    break
+                    m = m - increment
+                    increment = increment / 10
+                    if increment < 0.000001:
+                        break
                 else:
-                    m = m + 1
+                    m = m + increment
             _Normal.m1 = m
             _Normal.m2 = math.sqrt(((g - 1) * m ** 2 + 2) / ((2 * g * m ** 2) - (g - 1)))
             _Normal.p2p1 = (2 * g * m ** 2 - (g - 1)) / (g + 1)
